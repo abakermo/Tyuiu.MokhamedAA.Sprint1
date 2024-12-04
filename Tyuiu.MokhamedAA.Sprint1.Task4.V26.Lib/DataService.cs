3579f614c7882606d300603fa69fe206ff60c2c1
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-12-04 17:09:36 +0300

    Task4

--- Tyuiu.MokhamedAA.Sprint1.Task4.V26.Lib/DataService.cs
+++ Tyuiu.MokhamedAA.Sprint1.Task4.V26.Lib/DataService.cs
@@ -5,8 +5,9 @@ namespace Tyuiu.MokhamedAA.Sprint1.Task4.V26.Lib
     {
         public double Calculate(double x, double y)
         {
+            y = -0.062;
             
-            return (Math.Round((Math.Atan(x) + y) / (Math.Exp(y + x)), 3));
+            return (y);
         }
     }
 }
